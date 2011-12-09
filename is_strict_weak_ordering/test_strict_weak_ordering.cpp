3faf352bc9a943a97da6e0a15ed9cfd0872766ba
Jared Hoberock <jaredhoberock@gmail.com>
2011-12-09 14:29:36 -0800

    Tweak test_strict_weak_ordering.cpp

--- is_strict_weak_ordering/test_strict_weak_ordering.cpp
+++ is_strict_weak_ordering/test_strict_weak_ordering.cpp
@@ -13,17 +13,18 @@ int main()
   std::generate(int_vec.begin(), int_vec.end(), std::rand);
 
   // this should pass
-  std::cout << "is_strict_weak_ordering: " << is_strict_weak_ordering(float_vec.begin(), float_vec.end(), std::less<int>()) << std::endl;
+  std::cout << "is_strict_weak_ordering(int_vec): " << is_strict_weak_ordering(int_vec.begin(), int_vec.end(), std::less<int>()) << std::endl;
 
   
   std::vector<float> float_vec(1000);
 
   std::generate(float_vec.begin(), float_vec.end(), std::rand);
 
+  // add a NaN to make the test fail
   float_vec[5] = std::numeric_limits<float>::quiet_NaN();
 
   // this should fail
-  std::cout << "is_strict_weak_ordering: " << is_strict_weak_ordering(float_vec.begin(), float_vec.end(), std::less<float>()) << std::endl;
+  std::cout << "is_strict_weak_ordering(float_vec): " << is_strict_weak_ordering(float_vec.begin(), float_vec.end(), std::less<float>()) << std::endl;
 
   return 0;
 }
