e0d6e4e36ffd990494f74826bd506bc5d53c50bb
Jared Hoberock <jaredhoberock@gmail.com>
2011-12-09 13:48:36 -0800

    Add a test program for test_strict_weak_ordering.

---
+++ is_strict_weak_ordering/test_strict_weak_ordering.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+#include <functional>
+#include <cstdlib>
+#include <algorithm>
+#include <limits>
+#include "is_strict_weak_ordering.hpp"
+
+int main()
+{
+  std::vector<int> int_vec(1000);
+
+  std::generate(int_vec.begin(), int_vec.end(), std::rand);
+
+  // this should pass
+  std::cout << "is_strict_weak_ordering: " << is_strict_weak_ordering(float_vec.begin(), float_vec.end(), std::less<int>()) << std::endl;
+
+  
+  std::vector<float> float_vec(1000);
+
+  std::generate(float_vec.begin(), float_vec.end(), std::rand);
+
+  float_vec[5] = std::numeric_limits<float>::quiet_NaN();
+
+  // this should fail
+  std::cout << "is_strict_weak_ordering: " << is_strict_weak_ordering(float_vec.begin(), float_vec.end(), std::less<float>()) << std::endl;
+
+  return 0;
+}
+
