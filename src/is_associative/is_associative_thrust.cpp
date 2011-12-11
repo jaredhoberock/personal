d0cfb3793e4aef76003f9f22542a822e5b696a4b
Jared Hoberock <jaredhoberock@gmail.com>
2011-12-10 19:10:16 -0800

    Add thrust implementation of is_associative

---
+++ src/is_associative/is_associative_thrust.cpp
@@ -0,0 +1,45 @@
+#include <thrust/system/tbb/vector.h>
+#include <thrust/logical.h>
+
+// see http://en.wikipedia.org/wiki/Associativity#Definition
+
+template<typename ForwardIterator, typename BinaryFunction>
+  bool is_associative(ForwardIterator first, ForwardIterator last, BinaryFunction op)
+{
+  using namespace thrust;
+  
+  return all_of(first, last, [op,equals](value_type x){
+    all_of(first, last, [x,op,equals](value_type y){
+      all_of(first, last, [x,y,op,equals](value_type z){
+        return op(op(x,y), z) == op(x, op(y,z))
+      });
+    });
+  });
+}
+
+int main()
+{
+  const size_t n = 1000;
+
+  thrust::tbb::vector<int> int_vec(n);
+  thrust::tbb::vector<float> float_vec(n);
+
+  // use serial std::generate with rand
+  std::generate(int_vec.begin(), int_vec.end(), std::rand);
+  std::generate(float_vec.begin(), float_vec.end(), rand01);
+
+  // integer addition is associative
+  std::cout << "is_associative(int,plus): " << is_associative(int_vec.begin(), int_vec.end(), std::plus<int>()) << std::endl;
+
+  // integer subtraction is not associative
+  std::cout << "is_associative(int,minus): " << is_associative(int_vec.begin(), int_vec.end(), std::minus<int>()) << std::endl;
+
+  // floating point addition is not necessarily associative
+  std::cout << "is_associative(float,plus): " << is_associative(float_vec.begin(), float_vec.end(), std::plus<float>()) << std::endl;
+
+  // floating point addition may be "fuzzy" associative
+  std::cout << "is_associative(float,plus,fuzzy_equals): " << is_associative(float_vec.begin(), float_vec.end(), std::plus<float>(), fuzzy_equals) << std::endl;;
+
+  return 0;
+}
+
