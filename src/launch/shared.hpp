7539d8d9635454fa5147cb7ccc959dce2f4f1627
Jared Hoberock <jaredhoberock@gmail.com>
2012-01-13 16:56:46 -0800

    Allow friends of shared<T> to manage its lifetime

--- src/launch/shared.hpp
+++ src/launch/shared.hpp
@@ -1,15 +1,36 @@
 #pragma once
 
+#include <utility>
+
 template<typename T>
   class shared
 {
   public:
     shared(const shared &) = delete;
+    shared() = delete;
 
   private:
     template<typename> friend class launch_core_access;
 
-    shared() {}
+    // allows friends to construct dummy shared<T> objects
+    struct construct_dummy_tag {};
+    shared(construct_dummy_tag) {}
+
+    template<typename... Args>
+      shared(void *p, Args&&... args)
+        : ptr(reinterpret_cast<T*>(p))
+    {
+      // construct the object given the args
+      ::new(static_cast<void*>(ptr)) T(std::forward<Args>(args)...);
+    }
+
+    ~shared()
+    {
+      // destroy the object
+      ptr->~T();
+    }
+
+    T *ptr;
 };
 
 template<typename T>
@@ -17,7 +38,7 @@ template<typename T>
 {
   static shared<T> &shared_dummy()
   {
-    static shared<T> dummy;
+    static shared<T> dummy((typename shared<T>::construct_dummy_tag()));
     return dummy;
   }
 };
