edeb9b12fc761a338034d1930b85281db6cdf9b8
Jared Hoberock <jaredhoberock@gmail.com>
2012-04-19 17:23:47 -0700

    Check for num_threads > 0 in ucontext_cta's constructor.

--- src/barrier/ucontext_cta.hpp
+++ src/barrier/ucontext_cta.hpp
@@ -10,36 +10,37 @@ class ucontext_cta
       ucontext_cta(int num_threads, Function f)
         : cta()
     {
-      thread_state.clear();
-
-      // arguments to makecontext
-      // XXX each thread needs its own copy of f -- we should not refer to the same functor
-      void (*exec)(std::pair<ucontext_cta*,Function> *) = exec_thread<Function>;
-      std::pair<ucontext_cta*,Function> exec_parms = std::make_pair(this, f);
-
-      // save the return state
-      state join_state;
-      getcontext(&join_state);
-      if(thread_state.empty())
+      if(num_threads)
       {
-        thread_state.clear();
-        thread_state.resize(num_threads);
+        // arguments to makecontext
+        // XXX each thread needs its own copy of f -- we should not refer to the same functor
+        void (*exec)(std::pair<ucontext_cta*,Function> *) = exec_thread<Function>;
+        std::pair<ucontext_cta*,Function> exec_parms = std::make_pair(this, f);
 
-        for(int i = 0; i < thread_state.size(); ++i)
+        // save the return state
+        state join_state;
+        getcontext(&join_state);
+        if(thread_state.empty())
         {
-          getcontext(&thread_state[i]);
-          thread_state[i].uc_link = &join_state;
-          thread_state[i].uc_stack.ss_sp = thread_state[i].stack;
-          thread_state[i].uc_stack.ss_size = sizeof(thread_state[i].stack);
-          makecontext(&thread_state[i], (void(*)())exec, 1, &exec_parms);
+          thread_state.clear();
+          thread_state.resize(num_threads);
+
+          for(int i = 0; i < thread_state.size(); ++i)
+          {
+            getcontext(&thread_state[i]);
+            thread_state[i].uc_link = &join_state;
+            thread_state[i].uc_stack.ss_sp = thread_state[i].stack;
+            thread_state[i].uc_stack.ss_size = sizeof(thread_state[i].stack);
+            makecontext(&thread_state[i], (void(*)())exec, 1, &exec_parms);
+          }
+
+          // start thread 0
+          set_current_thread_id(0);
+          setcontext(&thread_state[0]);
         }
 
-        // start thread 0
-        set_current_thread_id(0);
-        setcontext(&thread_state[0]);
-      }
-
-      // when we've reached this point, all the threads in the group have terminated
+        // when we've reached this point, all the threads in the group have terminated
+      } // end if
 
       // null the current cta
       this_thread_group::__singleton = 0;
