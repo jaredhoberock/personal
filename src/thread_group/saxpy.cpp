6f90829342cb0e1d4417f9b96caef83668d57cfd
Jared Hoberock <jaredhoberock@gmail.com>
2012-04-25 17:08:23 -0700

    Add really slow saxpy demo

---
+++ src/thread_group/saxpy.cpp
@@ -0,0 +1,29 @@
+#include "thread_group.hpp"
+#include "async.hpp"
+
+void saxpy(float a, float *x, float *y, std::size_t n)
+{
+  int i = test::this_thread_group::get_id() * test::this_thread_group::size() + test::this_thread::get_id();
+
+  if(i < n)
+  {
+    x[i] = a * x[i] + y[i];
+  }
+}
+
+void my_saxpy(float a, float *x, float *y, std::size_t n)
+{
+  test::async(n, saxpy, a, x, y, n);
+}
+
+int main()
+{
+  std::size_t n = 1 << 20;
+  auto a = 1.0f;
+  std::vector<float> x(n), y(n);
+
+  my_saxpy(a, x.data(), y.data(), n);
+
+  return 0;
+}
+
