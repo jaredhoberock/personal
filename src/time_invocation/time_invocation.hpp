f0d20c087476b007d4a249ade9220a1c222ee620
Jared Hoberock <jaredhoberock@gmail.com>
2012-04-27 17:55:05 -0700

    Add utility for timing a function invocation

---
+++ src/time_invocation/time_invocation.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#if defined(__GNUC__) && defined(__GXX_EXPERIMENTAL_CXX0X__)
+#else
+#error "This file requires compiler support for c++11"
+#endif
+
+#include <cstddef>
+#include <chrono>
+
+template<typename Function, typename... Args>
+  double time_invocation(std::size_t num_trials, Function &&f, Args&&... args)
+{
+  auto start = std::chrono::high_resolution_clock::now();
+  for(std::size_t i = 0;
+      i < num_trials;
+      ++i)
+  {
+    f(args...);
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+
+  // return mean duration
+  return double((end - start).count()) / num_trials;
+}
+
