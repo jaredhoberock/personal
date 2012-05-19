6b8a8bb9fcacea77359a24d22f80b71f1d186108
Jared Hoberock <jaredhoberock@gmail.com>
2012-05-19 00:00:11 -0700

    Add timing for async launch through fcn ptr and functor

--- src/thread_group/saxpy.cpp
+++ src/thread_group/saxpy.cpp
@@ -4,7 +4,7 @@
 #include "async.hpp"
 #include "../time_invocation/time_invocation.hpp"
 
-void saxpy(float a, float *x, float *y, std::size_t n)
+inline void saxpy(float a, float *x, float *y, std::size_t n)
 {
   int i = test::this_thread_group::get_id() * test::this_thread_group::size() + test::this_thread::get_id();
 
@@ -14,9 +14,22 @@ void saxpy(float a, float *x, float *y, std::size_t n)
   }
 }
 
-void async_saxpy(float a, float *x, float *y, std::size_t n)
+void async_saxpy_fcn_ptr(float a, float *x, float *y, std::size_t n)
 {
   test::async(n, saxpy, a, x, y, n);
+}
+
+struct saxpy_group_task_functor
+{
+  void operator()(float a, float *x, float *y, std::size_t n)
+  {
+    saxpy(a,x,y,n);
+  }
+};
+
+void async_saxpy_functor(float a, float *x, float *y, std::size_t n)
+{
+  test::async(n, saxpy_group_task_functor(), a, x, y, n);
 }
 
 void serial_saxpy(float a, float *x, float *y, std::size_t n)
@@ -55,7 +68,8 @@ int main()
 
   std::cout << "serial_saxpy mean duration: " << time_invocation(1000, serial_saxpy, a, x.data(), y.data(), n) << std::endl;;
   std::cout << "serial_transform_saxpy mean duration: " << time_invocation(1000, serial_transform_saxpy, a, x.data(), y.data(), n) << std::endl;;
-  std::cout << "async_saxpy mean duration:  " << time_invocation(1000, async_saxpy, a, x.data(), y.data(), n) << std::endl;
+  std::cout << "async_saxpy_fcn_ptr mean duration:  " << time_invocation(1000, async_saxpy_fcn_ptr, a, x.data(), y.data(), n) << std::endl;
+  std::cout << "async_saxpy_functor mean duration:  " << time_invocation(1000, async_saxpy_functor, a, x.data(), y.data(), n) << std::endl;
 
   return 0;
 }
