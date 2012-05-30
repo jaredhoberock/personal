7b545b447ebe1311fb38f2e31aa8dd9f39a18834
Jared Hoberock <jaredhoberock@gmail.com>
2012-05-30 11:27:47 -0700

    Implement async with inplace_async for now

--- src/thread_group/detail/async_cpp03.hpp
+++ src/thread_group/detail/async_cpp03.hpp
@@ -4,6 +4,7 @@
 #include <tr1/type_traits>
 #include "schedule_cpp03.hpp"
 #include "parallel_for_async_cpp03.hpp"
+#include "inplace_async_cpp03.hpp"
 
 namespace test
 {
@@ -27,6 +28,13 @@ template<typename T>
 {};
 
 
+template<typename Function, typename Tuple>
+  void async_from_tuple(std::size_t num_groups, std::size_t num_threads, Function f, Tuple args)
+{
+//  return detail::parallel_for_async(num_groups, num_threads, f, args);
+  return detail::inplace_async(num_groups, num_threads, f, args);
+}
+
 }
 
 
@@ -34,35 +42,35 @@ template<typename Function>
   void async(std::size_t num_groups, std::size_t num_threads, Function f)
 {
   std::tr1::tuple<> args;
-  return detail::parallel_for_async(num_groups, num_threads, f, args);
+  return async_detail::async_from_tuple(num_groups, num_threads, f, args);
 } // end async()
 
 template<typename Function, typename Arg1>
   void async(std::size_t num_groups, std::size_t num_threads, Function f, Arg1 arg1)
 {
   std::tr1::tuple<Arg1> args(arg1);
-  return detail::parallel_for_async(num_groups, num_threads, f, args);
+  return async_detail::async_from_tuple(num_groups, num_threads, f, args);
 } // end async()
 
 template<typename Function, typename Arg1, typename Arg2>
   void async(std::size_t num_groups, std::size_t num_threads, Function f, Arg1 arg1, Arg2 arg2)
 {
   std::tr1::tuple<Arg1,Arg2> args(arg1,arg2);
-  return detail::parallel_for_async(num_groups, num_threads, f, args);
+  return async_detail::async_from_tuple(num_groups, num_threads, f, args);
 } // end async()
 
 template<typename Function, typename Arg1, typename Arg2, typename Arg3>
   void async(std::size_t num_groups, std::size_t num_threads, Function f, Arg1 arg1, Arg2 arg2, Arg3 arg3)
 {
   std::tr1::tuple<Arg1,Arg2,Arg3> args(arg1,arg2,arg3);
-  return detail::parallel_for_async(num_groups, num_threads, f, args);
+  return async_detail::async_from_tuple(num_groups, num_threads, f, args);
 } // end async()
 
 template<typename Function, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
   void async(std::size_t num_groups, std::size_t num_threads, Function f, Arg1 arg1, Arg2 arg2, Arg3 arg3, Arg4 arg4)
 {
   std::tr1::tuple<Arg1,Arg2,Arg3,Arg4> args(arg1,arg2,arg3,arg4);
-  return detail::parallel_for_async(num_groups, num_threads, f, args);
+  return async_detail::async_from_tuple(num_groups, num_threads, f, args);
 } // end async()
 
 
@@ -117,7 +125,7 @@ template<typename Function, typename Arg1, typename Arg2, typename Arg3, typenam
 {
   std::tr1::tuple<Arg1,Arg2,Arg3,Arg4,Arg5> args(arg1,arg2,arg3,arg4,arg5);
   std::pair<std::size_t,std::size_t> schedule = detail::schedule_async(num_threads, f, args);
-  return detail::parallel_for_async(schedule.first, schedule.second, f, args);
+  return async_detail::async_from_tuple(schedule.first, schedule.second, f, args);
 }
 
 
